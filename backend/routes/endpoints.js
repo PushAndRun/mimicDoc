b5cbf62450cd154a2e4c4a35a7f72001d63bf65a
jhschmid <jan-hendrik.schmid@campus.tu-berlin.de>
2021-04-23 21:11:19 +0200

    implemented some endpoints in router, logging and import of CSV

---
+++ backend/routes/endpoints.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var router = express.Router()
+const csv = require('csv-parser')
+const fs = require('fs')
+const diagnoses = './diagnoses_dict.csv'
+let csvData = []
+
+const loadDiagnoses = () => {
+    fs.createReadStream(diagnoses)
+    .pipe(csv())
+    .on('data', (data) => csvData.push(data))
+    .on('end', () => { console.log('csv diagnoses loaded')})
+}
+
+loadDiagnoses()
+
+router.get('/diagnoses', (req,res,next) => {
+    if(csvData){
+        res.send(csvData)
+        return
+    } else {
+        loadDiagnoses.then(res.send(csvData))
+    }
+})
+
+router.get('/searchdiagnose/:diagnose', (req,res,next) => {
+    let filteredDiags = csvData.filter(e => e.long_title.toLowerCase().includes(req.params.diagnose.toLowerCase()))
+    console.log(filteredDiags)
+    if(filteredDiags){
+        res.status(200).send(filteredDiags)
+    } else {
+        res.status(404).send('false')
+    }
+    return
+})
+
+router.get('/reloadDiagnoses', (req,res,next) => {
+    loadDiagnoses()
+    res.send('done')
+    return
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
+var logger = require('morgan')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
 // App
 const app = express();
+const endpointRouter = require('./routes/endpoints')
 
-app.get('/', express.static("./static"));
+app.use(logger('dev'))
+
+app.use('/',endpointRouter)
+app.get('/', express.static("./static"))
 
 app.get('/stay_prognose', (req, res, next) => {
   res.send("Thank you, next!");
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}`)
