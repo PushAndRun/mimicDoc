5a05a7dc8b2c979da73076c6f426af23ef063482
Anna Dumat <a.dumat@campus.tu-berlin.de>
2021-04-23 10:39:12 +0000

    Update Form.vue

--- frontend/components/Form.vue
+++ frontend/components/Form.vue
@@ -1,9 +1,50 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset">
+    <b-form id="formular" @submit="submitPatient" @reset="onReset">
+      <br>
+      <h2>Form for patient's assessment</h2>
+    <br>
+    <br>
+      
+
+      <b-form-group 
+      id="input-group-2" 
+      label="Patient's Name:"
+      label-for="input-2"
+      >
+      
+        <b-form-input
+          id="input-2"
+          type="text"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+       <b-form-group 
+      id="input-group-3" 
+      label="Patient's Birthdate Age:" 
+      label-for="input-3">
+        <b-form-datepicker
+          id="input-3"
+          v-model="form.age"
+          placeholder="Enter your birthday"
+          required
+        ></b-form-datepicker>
+      </b-form-group>
+
+      <b-form-group id="input-group-7"  label="Please choose patient's gender:" label-for="input-7" required>
+        
+          <b-form-radio v-model="form.gender" value="male">male</b-form-radio>
+          <b-form-radio v-model="form.gender" value="female">female</b-form-radio>
+
+        
+      </b-form-group>
+
       <b-form-group
         id="input-group-1"
-        label="Email address:"
+        label="Patient's Email address:"
         label-for="input-1"
         
         
@@ -17,38 +58,13 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group 
-      id="input-group-2" 
-      label="Your Name:"
-      label-for="input-2"
-      >
-      
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          placeholder="Enter name"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-       <b-form-group 
-      id="input-group-3" 
-      label="Your Age:" 
-      label-for="input-3">
-        <b-form-datepicker
-          id="input-3"
-          v-model="form.age"
-          placeholder="Enter your birthday"
-          required
-        ></b-form-datepicker>
-      </b-form-group>
-
        <b-form-group 
       id="input-group-4" 
-      label="Your Weight:" 
+      label="Patient's Weight in kg:" 
       label-for="input-4">
         <b-form-input
           id="input-4"
+          type="number"
           v-model="form.weight"
           placeholder="Enter weight"
           required
@@ -58,17 +74,18 @@
 
       <b-form-group 
       id="input-group-5" 
-      label="Your Height:" 
+      label="Patient's Height in cm:" 
       label-for="input-5">
         <b-form-input
+          type="number"
           id="input-5"
-          v-model="form.weight"
+          v-model="form.height"
           placeholder="Enter height"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-6" label="Please choose your Bloodtype:" label-for="input-6">
+      <b-form-group id="input-group-6" label="Please choose patient's Bloodtype:" label-for="input-6">
         <b-form-select
           id="input-6"
           v-model="form.bloodtype"
@@ -76,18 +93,11 @@
         ></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-7" v-slot="{ ariaDescribedby }" label="Please choose your gender:" label-for="input-7">
-        <b-form-checkbox-group
-          v-model="form.selected"
-          :options="options"
-          id="checkboxes-7"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="male">male</b-form-checkbox>
-          <b-form-checkbox value="female">female</b-form-checkbox>
-          <b-form-checkbox value="deverse">deverse</b-form-checkbox>
-        </b-form-checkbox-group>
+      <b-form-group id="input-group-8" label="Type in several patient's diagnoses and add with enter" label-for="input-8">
+        <b-form-tags v-model=form.diagnoses></b-form-tags>
       </b-form-group>
+
+      
 
       <b-button type="submit" variant="success">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
@@ -120,21 +130,18 @@
           weight: '',
           height: '',
           bloodtypes: null,
-          selected: [],
-           options: [
-          { text: 'male', value: 'male' },
-          { text: 'female', value: 'female' },
-          { text: 'deverse', value: 'diverse' }
-        ]
+          gender: '',
+          diagnoses:[],
+           
         },
         bloodtypes: [{ text: 'Unknown', value: null }, 'A+', 'A-', 'B+', 'B-' , 'AB+' , 'AB-' , '0+' , '0-'],
         
       }
     },
     methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
+      submitPatient (event){
+          event.preventDefault();
+          this.$emit("create-Patient", this.form);
       },
       onReset(event) {
         event.preventDefault()
@@ -146,6 +153,7 @@
         this.form.height = ''
         this.form.bloodtype = null
         this.form.gender = []
+        this.diagnoses=[]
        
       }
     }
@@ -164,6 +172,11 @@
 
 .btn {
   margin: 10px;
+}
+
+#formular {
+ margin-left:50px;
+ margin-right:100px; 
 }
 
 </style>
