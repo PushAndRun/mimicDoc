859c4258c878d3fd7bd7e7579b56184216cb3d2d
tgoldboom <tgoldboom@googlemail.com>
2021-04-28 18:39:01 +0200

    updated mimic exports

--- db/SqlQueries/DiagnosesDict.sql
+++ db/SqlQueries/DiagnosesDict.sql
@@ -1,6 +1,10 @@
-COPY (SELECT DISTINCT mimiciii.d_icd_diagnoses.icd9_code, short_title, long_title 
-FROM mimiciii.d_icd_diagnoses
-INNER JOIN mimiciii.procedures_icd 
-ON mimiciii.d_icd_diagnoses.icd9_code = mimiciii.procedures_icd.icd9_code
-ORDER BY mimiciii.d_icd_diagnoses.icd9_code ASC)
+COPY (		SELECT 	DISTINCT d_icd_diagnoses.icd9_code,
+	  				short_title,
+	  				long_title
+			FROM mimiciii.d_icd_diagnoses
+	  		INNER JOIN mimiciii.diagnoses_icd 
+			ON mimiciii.d_icd_diagnoses.icd9_code = mimiciii.diagnoses_icd.icd9_code
+			WHERE 	d_icd_diagnoses.icd9_code NOT LIKE '%V%' AND
+	 				d_icd_diagnoses.icd9_code NOT LIKE '%E%'
+	 		ORDER BY icd9_code ASC)
 TO 'C:\Users\Public\diagnoses_dict.csv' DELIMITER ',' CSV HEADER;
