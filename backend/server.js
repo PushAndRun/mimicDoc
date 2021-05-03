bc75cd35c14c0bf409c97a83bc9987d2c557f052
Leon <leon.rohmann@campus.tu-berlin.de>
2021-05-03 15:36:30 +0200

    started user management router

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const HOST = '0.0.0.0'
 const app = express();
 const endpointRouter = require('./routes/endpoints').router
 const requestRouter = require('./routes/request')
+const usertRouter = require('./routes/user_management.js')
 
 app.use(logger('dev'))
 app.use(
@@ -23,12 +24,8 @@ app.use(express.json())
 
 app.use('/',endpointRouter)
 app.use('/request',requestRouter)
+app.use('/user',requestRouter)
 app.get('/', express.static("./static"))
-
-
-app.get('/stay_prognose', (req, res, next) => {
-  res.send("Thank you, next!");
-});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`)
