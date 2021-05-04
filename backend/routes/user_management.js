56b352a4332a34fb3797d9d2c3232111398c5a27
Leon <leon.rohmann@campus.tu-berlin.de>
2021-05-04 19:47:48 +0200

    added find and delete for user management

--- backend/routes/user_management.js
+++ backend/routes/user_management.js
@@ -31,38 +31,11 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-//check if request is empty
-router.use('/',(req,res,next) => {
-    if(isEmptyObject(req.body)){
-        res.send(format)
-    } else {
-        next()
-    }
-})
-
-//check if request has required parameter keys
-router.use('/',(req,res,next) => {
-    console.log(Object.keys(format))
-    console.log(Object.keys(req.body))
-    missing = ''
-    for(let key of Object.keys(format)){
-        if(!Object.keys(req.body).includes(key)){
-            missing+=key+' '
-        }
-    }
-    if(missing !== ''){
-        res.send('Missing parameters: '+missing)
-    } else {
-        next()
-    }
-})
-
-//TODO: Register
-
-router.use('/register',(req,res,next) => {
-    console.log("registering");
-    // check if user is already registered
-
+// search for user
+router.get('/find/:username',(req,res,next) => {
+    let uname = req.params.username.toLowerCase();
+    console.log("searching user");
+    console.log(uname);
     mongoose.connect(user_db_uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -70,17 +43,8 @@ router.use('/register',(req,res,next) => {
         useCreateIndex: true
     }).then(() => {
         console.log('MongoDB connected!!');
-        /* const user = new User({
-            username: req.body.username,
-            pw: req.body.pw
-        }); */
-        /* user.save().then(() => {
-            console.log('User successfully added.');
-        }).catch(err => {
-            console.log('Failed to add user', err)
-        }) */
         User.find({
-            username: req.body.username
+            username: uname
         }).then(() => {
             console.log('User found.');
         }).catch(err => {
@@ -92,14 +56,91 @@ router.use('/register',(req,res,next) => {
     next();
 });
 
+//check if request is empty
+router.use('/register',(req,res,next) => {
+    if(isEmptyObject(req.body)){
+        res.send(format)
+    } else {
+        next()
+    }
+})
+
+//check if request has required parameter keys
+router.use('/register',(req,res,next) => {
+    console.log(Object.keys(format))
+    console.log(Object.keys(req.body))
+    missing = ''
+    for(let key of Object.keys(format)){
+        if(!Object.keys(req.body).includes(key)){
+            missing+=key+' '
+        }
+    }
+    if(missing !== ''){
+        res.send('Missing parameters: '+missing)
+    } else {
+        next();
+    }
+})
+//TODO: Register
+router.post('/register',(req,res,next) => {
+    console.log("registering");
+    // check if user is already registered
+    mongoose.connect(user_db_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(() => {
+        console.log('MongoDB connected!!');
+        
+        const user = new User({
+            username: req.body.username,
+            pw: req.body.pw
+        });
+        user.save().then(() => {
+            console.log('User successfully added.');
+        }).catch(err => {
+            console.log('Failed to add user', err)
+        }) 
+    }).catch(err => {
+        console.log('Failed to connect to MongoDB', err);
+    });
+    next();
+});
+
 //TODO: Login
 
 //TODO: more user functionality. e.g. list patients, show request history
+// delete all users with username - not case sensitive
+router.get('/delete/:username',(req,res,next) => {
+    let uname = req.params.username.toLowerCase();
+    console.log("searching user");
+    console.log(uname);
+    mongoose.connect(user_db_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(() => {
+        console.log('MongoDB connected!!');
+        User.find({
+            username: { $regex: uname , $options: 'i'}
+        }).deleteMany(
 
+        ).then(() => {
+            console.log('User deleted.');
+        }).catch(err => {
+            console.log('User not found.', err)
+        });
+    }).catch(err => {
+        console.log('Failed to connect to MongoDB', err);
+    });
+    next();
+});
 
 //Pseudo-Output
-/* router.use('/',(req,res,next) => {
-    res.send(`under construction`)
-}); */
+router.use('/',(req,res,next) => {
+    res.status(200).send("under construction");
+});
 
 module.exports = router;
