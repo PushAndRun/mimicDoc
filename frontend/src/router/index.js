e1ed950fdbf51cbc3ccdbdef87cbdacedd299302
Anna Dumat <a.dumat@campus.tu-berlin.de>
2021-05-10 08:56:27 +0000

    fixed path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Form from '@/components/Form'
-import Registration from '@/components/Registration'
-import Patients from '@/components/Patients'
+import Form from '../components/Form'
+import Registration from '../components/Registration'
+import Patients from '../components/Patients'
 
 Vue.use(Router)
 
