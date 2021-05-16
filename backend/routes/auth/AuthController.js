cf4b0b1c784328a5a6494e430b5c8d61d2c11423
Jan-Hendrik <jnhndrk01@gmail.com>
2021-05-16 11:34:12 +0200

    Auth fixed on Proto due to old db schema on server

--- backend/routes/auth/AuthController.js
+++ backend/routes/auth/AuthController.js
@@ -48,14 +48,17 @@ router.get('/logout', function(req, res) {
 router.post('/register', function(req, res) {
 
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
+  console.log(req.body)
   User.create({
     name : req.body.name,
     email : req.body.email,
     password : hashedPassword
   }, 
   function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user`.");
+    if (err) {
+      console.log(err)
+      return res.status(500).send("There was a problem registering the user`.");
+    }
 
     // if user is registered without errors
     // create a token
