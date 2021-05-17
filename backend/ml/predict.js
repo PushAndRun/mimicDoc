0bb71b49efa11710f3eef9ae744facd6d04f5b80
proxycon <leon.rohmann@campus.tu-berlin.de>
2021-05-17 12:47:20 +0200

    added python runner

---
+++ backend/ml/predict.js
@@ -0,0 +1,18 @@
+
+function predict(params) {
+    // spawn new child process to call the python script
+    const python = spawn('python', ['ml_predict.py', params]);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    return dataToSend;
+    });
+}
+
+module.exports = predict
