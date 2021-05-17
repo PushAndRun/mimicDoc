e8e19c0a8503932f7d1660b7845a9ba5e247ce54
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2021-05-17 16:45:40 +0200

    python finally running

---
+++ backend/routes/predict.js
@@ -0,0 +1,34 @@
+
+const {PythonShell} =require('python-shell');
+
+async function predict(params) {
+    console.log('running ml script with '+params);
+    let dataToSend = [];
+
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: 'ml/', //If you are having python_test.py script in same folder, then it's optional.
+        args: [params] //An argument which can be accessed in the script using sys.argv[1]
+    };
+    
+    let runPy = new Promise(function(success, nosuccess) {
+        let shell = new PythonShell('predict.py', options);
+        shell.on('error', function (error) {
+            console.error("ml error: "+stderr);
+            nosuccess(stderr);
+        });
+        shell.on('message', function (message) {
+           dataToSend.push(message);
+        });
+        shell.on('close', (message) => {
+            success(dataToSend);
+         });
+    });
+    
+
+    let res = await runPy;
+    return dataToSend;
+}
+
+module.exports = predict;
