75c5f5066440d15d406298e995ed417a57733c58
proxycon <leon.rohmann@campus.tu-berlin.de>
2021-05-17 23:16:42 +0200

    merge?

--- backend/routes/request/request.js
+++ backend/routes/request/request.js
@@ -6,7 +6,11 @@ const DiagnosisModel = require('./DiagnosisModel')
 const Patient = require('../patients/PatientModel')
 const RequestModel = require('./RequestModel')
 const verifyToken = require('../auth/VerifyToken')
+<<<<<<< HEAD
 const predict = require('../predict')
+=======
+//const predict = require('../../ml/predict')
+>>>>>>> 16b84403ae527617a37a0f2319a7a4c4148c5096
 
 function isEmptyObject(obj) {
     return !Object.keys(obj).length;
@@ -61,10 +65,17 @@ router.post('/', verifyToken, async (req,res,next) => {
     }
 
     // TODO: Prediction von ML einholen - placeholder for now
+<<<<<<< HEAD
     let prediction = await predict("76,M,97,76,40,259,5,24,17,37.002880708670915,136,306,232,,0389;78559;5849;4275;41071;4280;6826;4254;2639");
     console.log("prediction: death:"+prediction[0]+" - stay: "+prediction[1]);
     let death_prediction = prediction[0];
     let stay_prediction = prediction[1];
+=======
+    // let prediction = predict.predict("76,M,97,76,40,259,5,24,17,37.002880708670915,136,306,232,,0389;78559;5849;4275;41071;4280;6826;4254;2639").split("\n");
+    let prediction = [0,14]   //pseudo
+    let death_prediction = prediction[0]
+    let stay_prediction = prediction[1]
+>>>>>>> 16b84403ae527617a37a0f2319a7a4c4148c5096
 
     // Request in der DB ablegen
     RequestModel.create({
