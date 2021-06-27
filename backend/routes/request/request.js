5bb5e67de4771873353bc3baabe7f7eedbd36319
Bourryto 🚀 <bourryto@campus.tu-berlin.de>
2021-06-28 00:07:41 +0200

    added missing params, changed order to mach admissions.csv

--- backend/routes/request/request.js
+++ backend/routes/request/request.js
@@ -87,28 +87,107 @@ router.post('/', verifyToken, async (req, res, next) => {
     // 0389;78559;5849;4275;41071;4280;6826;4254;2639");
     // format age,gender,weight,meanbp_mean,meanbp_min,meanbp_max,resprate_min,resprate_max,resprate_mean,tempc_mean,glucose_min,glucose_max,glucose_mean,patient_history,diagnoses
 
-    // format:  hospstay_seq,total_hospstays,length_of_stay_hospital,length_of_stay_icu,total_length_of_stay_icu
-    //          days_to_death,age,weight,height,heartrate_mean,heartrate_min,heartrate_max,meanbp_mean,meanbp_min,meanbp_max
-    //          resprate_mean,resprate_min,resprate_max,tempc_mean,tempc_min,tempc_max,spo2_mean,spo2_min,spo2_max
-    //          glucose_mean,glucose_min,glucose_max,received_dialysis,received_ventilation,urineoutput,mingcs
-    //          gcsmotor,gcsverbal,gcseyes,aniongap_min,aniongap_max,albumin_min,albumin_max,bands_min,bands_max
-    //          bicarbonate_min,bicarbonate_max,bilirubin_min,bilirubin_max,creatinine_min,creatinine_max
-    //          chloride_min,chloride_max,hematocrit_min,hematocrit_max,hemoglobin_min,hemoglobin_max,lactate_min,lactate_max
-    //          platelet_min,platelet_max,potassium_min,potassium_max,ptt_min,ptt_max,inr_min,inr_max,pt_min,pt_max
-    //          sodium_min,sodium_max,bun_min,bun_max,wbc_min,wbc_max
+    // format: 
+                // patient_id
+                // hadm_id
+                // icustay_id
+                // hospstay_seq
+                // total_hospstays
+                // length_of_stay_hospital
+                // icustay_seq
+                // number_of_icu_stays
+                // length_of_stay_icu
+                // total_length_of_stay_icu
+                // days_to_death
+                // died_in_hospital
+                // age
+                // gender
+                // weight
+                // height
+                // heartrate_mean
+                // heartrate_min
+                // heartrate_max
+                // meanbp_mean
+                // meanbp_min
+                // meanbp_max
+                // resprate_mean
+                // resprate_min
+                // resprate_max
+                // tempc_mean
+                // tempc_min
+                // tempc_max
+                // spo2_mean
+                // spo2_min
+                // spo2_max
+                // glucose_mean
+                // glucose_min
+                // glucose_max
+                // received_dialysis
+                // received_ventilation
+                // urineoutput
+                // mingcs
+                // gcsmotor
+                // gcsverbal
+                // gcseyes
+                // aniongap_min
+                // aniongap_max
+                // albumin_min
+                // albumin_max
+                // bands_min
+                // bands_max
+                // bicarbonate_min
+                // bicarbonate_max
+                // bilirubin_min
+                // bilirubin_max
+                // creatinine_min
+                // creatinine_max
+                // chloride_min
+                // chloride_max
+                // hematocrit_min
+                // hematocrit_max
+                // hemoglobin_min
+                // hemoglobin_max
+                // lactate_min
+                // lactate_max
+                // platelet_min
+                // platelet_max
+                // potassium_min
+                // potassium_max
+                // ptt_min
+                // ptt_max
+                // inr_min
+                // inr_max
+                // pt_min
+                // pt_max
+                // sodium_min
+                // sodium_max
+                // bun_min
+                // bun_max
+                // wbc_min
+                // wbc_max
+                // symptoms
+                // patient_history
+                // accident_causes
+                // diagnoses
+    
 
-
-    let prediction = await predict(",," +
+    let prediction = await predict("" +
+        req.body.patient.patient_id + "," +
+        req.body.patient.medicalData.hadm_id + "," +
+        req.body.patient.medicalData.icustay_id + "," +
         req.body.patient.medicalData.hospstay_seq + "," +
-        req.body.patient.medicalData.total_hospstays + "," + // not shure how total should be calculated?
-        req.body.patient.medicalData.length_of_stay_hospital + ",,,," +
+        req.body.patient.medicalData.total_hospstays + "," +
+        req.body.patient.medicalData.length_of_stay_hospital + "," +
+        req.body.patient.medicalData.icustay_seq + "," +
+        req.body.patient.medicalData.number_of_icu_stays + "," +
         req.body.patient.medicalData.length_of_stay_icu + "," +
-        req.body.patient.medicalData.total_length_of_stay_icu + "," + // not shure how total should be calculated?
-        req.body.patient.medicalData.days_to_death + ",," +
+        req.body.patient.medicalData.total_length_of_stay_icu + "," +
+        req.body.patient.medicalData.days_to_death + "," +
+        req.body.patient.medicalData.died_in_hospital + "," +
         calculateAge(newPatient.medicalData.dateOfBirth).toString() + "," +
-        newPatient.medicalData.gender + "," +
-        newPatient.medicalData.weight + "," +
-        newPatient.medicalData.height + "," +
+        req.body.patient.gender + "," +
+        req.body.patient.weight + "," +
+        req.body.patient.height + "," +
         req.body.patient.medicalData.heartrate.mean + "," +
         req.body.patient.medicalData.heartrate.min + "," +
         req.body.patient.medicalData.heartrate.max + "," +
@@ -173,7 +252,7 @@ router.post('/', verifyToken, async (req, res, next) => {
         req.body.patient.medicalData.symptoms + "," +
         req.body.patient.medicalData.patient_history + "," +
         req.body.patient.medicalData.accident_causes + "," +
-        req.body.patient.medicalData.diagnoses 
+        req.body.patient.medicalData.diagnoses
     );
     
     /* 
@@ -202,21 +281,27 @@ router.post('/', verifyToken, async (req, res, next) => {
 
     // Request in der DB ablegen
     RequestModel.create({
+        patient: newPatient._id,
+        created: new Date(),
         survival: death_prediction,
         stay: stay_prediction,
-        glucose: {
-            glucose_min: req.body.patient.medicalData.glucose.min,
-            glucose_max: req.body.patient.medicalData.glucose.max,
-            glucose_mean: req.body.patient.medicalData.glucose.mean
-        },
 
+        hadm_id: req.body.patient.medicalData.hadm_id,
+        icustay_id: req.body.patient.medicalData.icustay_id,
         hospstay_seq: req.body.patient.medicalData.hospstay_seq,
         total_hospstays: req.body.patient.medicalData.total_hospstays,
         length_of_stay_hospital: req.body.patient.medicalData.length_of_stay_hospital,
+        icustay_seq: req.body.patient.medicalData.icustay_seq,
+        number_of_icu_stays: req.body.patient.medicalData.number_of_icu_stays,
         length_of_stay_icu: req.body.patient.medicalData.length_of_stay_icu,
         total_length_of_stay_icu: req.body.patient.medicalData.total_length_of_stay_icu,
         days_to_death: req.body.patient.medicalData.days_to_death,
+        died_in_hospital: req.body.patient.medicalData.died_in_hospital,
+
         age: calculateAge(newPatient.medicalData.dateOfBirth),
+        gender: req.body.patient.gender,
+        weight: req.body.patient.weight,
+        height: req.body.patient.height,
 
         heartrate:{
             mean: req.body.patient.medicalData.heartrate.mean,
@@ -243,13 +328,21 @@ router.post('/', verifyToken, async (req, res, next) => {
             min: req.body.patient.medicalData.spo2.min,
             max: req.body.patient.medicalData.spo2.max
         },
+        glucose: {
+            glucose_min: req.body.patient.medicalData.glucose.min,
+            glucose_max: req.body.patient.medicalData.glucose.max,
+            glucose_mean: req.body.patient.medicalData.glucose.mean
+        },
+
         received_dialysis: req.body.patient.medicalData.received_dialysis,
         received_ventilation: req.body.patient.medicalData.received_ventilation,
+        
         urineoutput: req.body.patient.medicalData.urineoutput,
         mingcs: req.body.patient.medicalData.mingcs,
         gcsmotor: req.body.patient.medicalData.gcsmotor,
         gcsverbal: req.body.patient.medicalData.gcsverbal,
         gcseyes: req.body.patient.medicalData.gcseyes,
+        
         aniongap: {
             min: req.body.patient.medicalData.aniongap.min,
             max: req.body.patient.medicalData.aniongap.max
@@ -322,10 +415,12 @@ router.post('/', verifyToken, async (req, res, next) => {
             min: req.body.patient.medicalData.wbc.min,
             max: req.body.patient.medicalData.wbc.max
         },
+
+        symptoms: req.body.patient.medicalData.symptoms,
         patient_history: req.body.patient.medicalData.patient_history,
-        diagnoses: req.body.patient.medicalData.diagnoses,
-        created: new Date(),
-        patient: newPatient._id
+        accident_causes: req.body.patient.medicalData.accident_causes,
+        diagnoses: req.body.patient.medicalData.diagnoses
+
     },function (err, request) {
         if (err) {
             console.log("err:"+err)
